4c55c680dcc9f197277e1f2a65ce624fa87eac44
Tung Dang <tungds@vimo.vn>
2024-04-10 10:00:42 +0700

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,14 +5,10 @@ export const MISSION_STATUS = {
 };
 
 export const TIME_TO_SLEEP = 30000;
-
-// export const BASE_URL = 'http://192.168.68.111:5000'
 export const BASE_URL = 'http://192.168.68.105:3000'
-// export const BASE_URL = 'http://127.0.0.1:3000'
 export const STATIONS = '/locals'
 export const TASKS = '/scripts'
 export const AGV_INFO = '/status'
-// export const BASE_WEBSOCKET_URL = 'ws://192.168.68.111:8765'
 export const BASE_WEBSOCKET_URL = 'ws://192.168.68.105:3000'
 export const MISSION_PROGRESS = '/esatech/run'
 
