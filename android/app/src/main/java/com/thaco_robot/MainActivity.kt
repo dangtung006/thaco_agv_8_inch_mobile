0db6e27d836c91528487462417eb22b0cec70c5c
luongduy2798 <nhatduy2727@gmail.com>
2024-06-28 21:26:23 +0700

    remove status bar from native

--- android/app/src/main/java/com/thaco_robot/MainActivity.kt
+++ android/app/src/main/java/com/thaco_robot/MainActivity.kt
@@ -5,12 +5,21 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 import android.os.Bundle;
+import android.view.View;
+import android.os.Build
 
 
 class MainActivity : ReactActivity() {
   
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      window.setDecorFitsSystemWindows(false)
+    } else {
+      window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+    }
+    actionBar?.hide()
+
   }
 
   /**
